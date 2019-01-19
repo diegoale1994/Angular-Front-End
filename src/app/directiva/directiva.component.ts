7c9bbbf39c2aae47b3ffe9e2e0eece95c205659f
diegoale1994 <diego.fc.1@hotmail.com>
2019-01-19 13:18:28 -0500

    Client rest getting data integrated

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+  listaCurso: string[] = ['TypeScript','Javascript','Java SE','C#'];
+habilitar: boolean=true;
+  constructor() { }
+  setHabilitar(): void{
+    this.habilitar=(this.habilitar==true)? false:true
+  }
+}
