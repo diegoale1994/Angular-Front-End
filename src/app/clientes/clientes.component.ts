12f4ee31696a2da2e8ce93d34457df96e65601bb
diegoale1994 <diego.fc.1@hotmail.com>
2019-01-26 11:58:23 -0500

    crud ready, all services full

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
 import {ClienteService} from './cliente.service';
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
@@ -12,4 +13,35 @@ export class ClientesComponent implements OnInit {
   ngOnInit() {
     this.clientesService.getClientes().subscribe(clientes => this.clientes = clientes);
   }
+
+  delete (cliente: Cliente): void{
+    Swal({
+      title: 'Esta seguro?',
+      text: `Esta seguro que desea eliminar a ${cliente.nombre} ?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!'
+    }).then((result) => {
+      if (result.value) {
+
+          this.clientesService.delete(cliente.id).subscribe(
+            response => {
+
+                this.clientes = this.clientes.filter(cli => cli !== cliente)
+
+              Swal(
+                'Eliminado!',
+                'Cliente eliminado',
+                'success'
+                  )
+            }
+          )
+
+
+}
+})
+  }
+
 }
---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente'
+import {ClienteService} from './cliente.service'
+import {Router, ActivatedRoute} from '@angular/router'
+import Swal from 'sweetalert2'
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent implements OnInit {
+  private cliente: Cliente = new Cliente();
+  private titulo: string = "Crear Cliente";
+
+  constructor(private ClienteService: ClienteService,
+    private router: Router,
+  private ActivatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.cargarCliente();
+  }
+
+  cargarCliente(): void{
+    this.ActivatedRoute.params.subscribe(params =>{
+      let id = params['id']
+
+      if (id) {
+        this.ClienteService.getCliente(id).subscribe( (cliente)=> this.cliente = cliente)
+      }
+
+    })
+
+  }
+
+ create(): void {
+    this.ClienteService.create(this.cliente)
+    .subscribe(cliente => {
+        this.router.navigate(['/clientes'])
+        Swal('Nuevo Cliente', `Cliente ${cliente.nombre} creado con exito!`, 'success')
+    }
+  );
+  }
+
+  update():void{
+    this.ClienteService.update(this.cliente)
+    .subscribe(cliente =>{
+        this.router.navigate(['/clientes'])
+        Swal('Cliente Actuaizado',`Cliente ${cliente.nombre} Actualizado correctamente`,'success')
+      }
+
+    )
+  }
+
+}
